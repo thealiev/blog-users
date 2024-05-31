d761205591a153c218131ea2201c45c0ff0a5930
Salohiddin Tojiyev <129267512+thealiev@users.noreply.github.com>
2024-06-01 01:08:44 +0500

    addd new url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:3002'
+axios.defaults.baseURL = "https://blog-user-backend.onrender.com";
 
 axios.interceptors.request.use(config => {
     const token = localStorage.getItem('token')
